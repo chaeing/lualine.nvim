49772c9e1eba4f136ec1b1a3cad657e9da5ff066
Chae SM <smchae9@gmail.com>
2021-08-20 04:41:14 -0700

    feat: add fallback 'TEXT' when filetype not found

--- lua/lualine/components/filetype.lua
+++ lua/lualine/components/filetype.lua
@@ -1,62 +1,71 @@
 -- Copyright (c) 2020-2021 hoob3rt
 -- MIT license, see LICENSE for more details.
-local highlight = require('lualine.highlight')
-local utils = require('lualine.utils.utils')
+local highlight = require("lualine.highlight")
+local utils = require("lualine.utils.utils")
 
-local FileType = require('lualine.component'):new()
+local FileType = require("lualine.component"):new()
 
 function FileType:new(options, child)
-  local new_instance = self._parent:new(options, child or FileType)
-  if new_instance.options.colored == nil then
-    new_instance.options.colored = true
-  end
-  if new_instance.options.disable_text == nil then
-    new_instance.options.disable_text = false
-  end
-  return new_instance
+	local new_instance = self._parent:new(options, child or FileType)
+	if new_instance.options.colored == nil then
+		new_instance.options.colored = true
+	end
+	if new_instance.options.disable_text == nil then
+		new_instance.options.disable_text = false
+	end
+	return new_instance
 end
 
-function FileType.update_status() return vim.bo.filetype or '' end
+function FileType.update_status()
+	local filetype = vim.bo.filetype
+	if filetype == "" then
+		return "TEXT"
+	end
+	return filetype
+end
 
 function FileType:apply_icon()
-  if not self.options.icons_enabled then return end
+	if not self.options.icons_enabled then
+		return
+	end
 
-  local icon, icon_highlight_group
-  local ok, devicons = pcall(require, 'nvim-web-devicons')
-  if ok then
-    local f_name, f_extension = vim.fn.expand('%:t'), vim.fn.expand('%:e')
-    icon, icon_highlight_group = devicons.get_icon(f_name, f_extension)
+	local icon, icon_highlight_group
+	local ok, devicons = pcall(require, "nvim-web-devicons")
+	if ok then
+		local f_name, f_extension = vim.fn.expand("%:t"), vim.fn.expand("%:e")
+		icon, icon_highlight_group = devicons.get_icon(f_name, f_extension)
 
-    if icon and self.options.colored then
-      local highlight_color = utils.extract_highlight_colors(
-                                  icon_highlight_group, 'fg')
-      local is_focused = utils.is_focused()
-      local default_highlight = highlight.format_highlight(is_focused,
-                                                           self.options.self
-                                                               .section)
-      local icon_highlight = self.options.self.section .. '_' ..
-                                 icon_highlight_group
-      if not utils.highlight_exists(icon_highlight .. '_normal') then
-        icon_highlight = highlight.create_component_highlight_group(
-                             {fg = highlight_color}, icon_highlight_group,
-                             self.options)
-      end
+		if icon and self.options.colored then
+			local highlight_color = utils.extract_highlight_colors(icon_highlight_group, "fg")
+			local is_focused = utils.is_focused()
+			local default_highlight = highlight.format_highlight(is_focused, self.options.self.section)
+			local icon_highlight = self.options.self.section .. "_" .. icon_highlight_group
+			if not utils.highlight_exists(icon_highlight .. "_normal") then
+				icon_highlight = highlight.create_component_highlight_group(
+					{ fg = highlight_color },
+					icon_highlight_group,
+					self.options
+				)
+			end
 
-      icon = highlight.component_format_highlight(icon_highlight) .. icon ..
-                 default_highlight
-    end
-  else
-    ok = vim.fn.exists('*WebDevIconsGetFileTypeSymbol')
-    if ok ~= 0 then icon = vim.fn.WebDevIconsGetFileTypeSymbol() end
-  end
+			icon = highlight.component_format_highlight(icon_highlight) .. icon .. default_highlight
+		end
+	else
+		ok = vim.fn.exists("*WebDevIconsGetFileTypeSymbol")
+		if ok ~= 0 then
+			icon = vim.fn.WebDevIconsGetFileTypeSymbol()
+		end
+	end
 
-  if not icon then return end
+	if not icon then
+		return
+	end
 
-  if self.options.disable_text then
-    self.status = icon
-  else
-    self.status = icon .. ' ' .. self.status
-  end
+	if self.options.disable_text then
+		self.status = icon
+	else
+		self.status = icon .. " " .. self.status
+	end
 end
 
 return FileType
